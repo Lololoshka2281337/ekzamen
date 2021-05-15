3c5eb35e5dcb570fd901e204cf5cf3a9512d3e93
Lololoshka2281337 <84225526+Lololoshka2281337@users.noreply.github.com>
2021-05-15 12:50:36 +0300

    Add files via upload

---
+++ ekzamen/ekzamen/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ekzamen
+{
+    class Class1
+    {
+        public void zadanie3()
+        {
+            
+            double max;
+            Console.WriteLine("Введите N: ");
+            int N = Int32.Parse(Console.ReadLine());
+            double[] a = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                a[i] = Double.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("Максимальный элемент последовательности: {0}", a.Max());
+        }
+    }
+}
---
+++ ekzamen/ekzamen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ekzamen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.zadanie3();
+        }
+    }
+}
